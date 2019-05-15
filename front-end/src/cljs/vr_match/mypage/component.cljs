cc69a9629431f49590878b9f8d176dafddc170f4
boxp <tiyotiyouda@gmail.com>
2019-05-15 23:28:58 +0900

    メイン画像の編集ボタンスタイル調整

--- front-end/src/cljs/vr_match/mypage/component.cljs
+++ front-end/src/cljs/vr_match/mypage/component.cljs
@@ -31,22 +31,24 @@
         image-ref (r/atom nil)]
     (fn []
       [navigation-bar-layout {:title "プロフィールを編集"}
-       [:<>
+       [:div {:style {:position "relative"}}
         [mui/button-base {:on-click #(handle-click-reset-image image-ref)
-                          :style {:position "relative"
-                                  :width "100vw"
+                          :style {:width "100vw"
                                   :height "100vw"}}
          [:img {:src @draft-image
                 :style {:width "100vw"
-                        :height "100vw"}}]
-         [mui/avatar {:style {:position "absolute"
-                              :width "56px"
-                              :height "56px"
-                              :top "16px"
-                              :right "16px"
-                              :background-color mui/primary-color}}
-          [mui/icon {:font-size "large"}
-           "edit"]]]
+                        :height "100vw"}}]]
+        [mui/icon-button {:on-click #(handle-click-reset-image image-ref)
+                          :style
+                          {:position "absolute"
+                           :width "64px"
+                           :height "64px"
+                           :top "calc(100vw - 40px)"
+                           :right "16px"
+                           :background-color mui/primary-color}}
+         [mui/icon {:font-size "large"
+                    :style {:color "white"}}
+          "edit"]]
         [:input {:type "file"
                  :on-change #(handle-change-image draft-image %)
                  :ref (fn [com] (reset! image-ref com))
