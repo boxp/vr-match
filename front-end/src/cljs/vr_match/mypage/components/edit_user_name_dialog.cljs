b5cd2bf5996a90e9e59ab39b0cfeeb41c78859f1
boxp <tiyotiyouda@gmail.com>
2019-05-17 00:15:07 +0900

    ユーザー名編集Dialog実装

---
+++ front-end/src/cljs/vr_match/mypage/components/edit_user_name_dialog.cljs
@@ -0,0 +1,40 @@
+(ns vr-match.mypage.components.edit-user-name-dialog
+  (:require
+   [cljs.spec.alpha :as s]
+   [reagent.core :as r]
+   [vr-match.lib.components.material-ui :as mui]))
+
+(s/def ::isOpen boolean?)
+(s/def ::userName string?)
+(s/def ::handleClickSubmit fn?)
+(s/def ::handleCancel fn?)
+(s/fdef edit-user-name-dialog
+  :args (s/cat :props (s/keys :req-un [::isOpen
+                                       ::userName
+                                       ::handleClickSubmit
+                                       ::handleCancel]))
+  :ret vector?)
+(defn edit-user-name-dialog
+  [props]
+  (let [draft-user-name (r/atom (-> props :userName))]
+    (fn [{:keys [isOpen
+                 userName
+                 handleSubmit
+                 handleCancel]}]
+      [mui/dialog {:open isOpen
+                   :onClose handleCancel
+                   :aria-labelledby "ユーザー名を入力してください"}
+       [mui/dialog-title "ユーザー名を入力してください"]
+       [mui/dialog-content
+        [mui/text-field {:autoFocus true
+                         :margin "dense"
+                         :label "ユーザー名"
+                         :type "text"}]]
+       [mui/dialog-actions
+        [mui/button {:on-click handleCancel}
+         "キャンセル"]
+        [mui/button {:on-click handleSubmit}
+         "決定"]]])))
+
+
+
