6ac4ce3fd14746858242bd3d80d1107c7003476e
boxp <tiyotiyouda@gmail.com>
2019-07-11 02:47:39 +0900

    fix: imageにplaceholder-colorを追加しわすれていた

--- back-end/src/vr_match_back_end/domain/entity/image.clj
+++ back-end/src/vr_match_back_end/domain/entity/image.clj
@@ -1,8 +1,11 @@
 (ns vr-match-back-end.domain.entity.image
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [clj-time.spec :as t-spec]))
 
 (s/def ::url string?)
 (s/def ::type #{:main :sub})
+(s/def ::placeholder-color string?)
 
 (s/def ::image
-  (s/keys :req-un [::url ::type]))
+  (s/keys :req-un [::url ::type]
+          :opt-un [::placeholder-color]))
