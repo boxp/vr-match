87cfed0c4a08458e27bdd58b1062fa9b0e75e48b
boxp <tiyotiyouda@gmail.com>
2019-07-15 12:12:37 +0900

    fix: mysql datasourceのspecがなかった

--- back-end/src/vr_match_back_end/infra/datasource/mysql.clj
+++ back-end/src/vr_match_back_end/infra/datasource/mysql.clj
@@ -1,7 +1,18 @@
 (ns vr-match-back-end.infra.datasource.mysql
   (:require
+   [clojure.spec.alpha :as s]
    [clj-time.jdbc]
    [com.stuartsierra.component :as component]))
+
+(s/def ::dbname string?)
+(s/def ::user string?)
+(s/def ::password string?)
+(s/def :db/dbtype string?)
+(s/def ::db
+  (s/keys :req-un [::dbname ::user ::password :db/dbtype]))
+(s/def ::mysql-datasource
+  (s/keys :req-un [::dbname ::user ::password]
+          :opt-un [::db]))
 
 (defn init-db
   [{:keys [dbname user password] :as params}]
