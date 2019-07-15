6c8975142e1e1e0986cc7523708e58f5a10c8818
boxp <tiyotiyouda@gmail.com>
2019-07-15 12:31:42 +0900

    platform master, platformに必要なprop追加

--- back-end/src/vr_match_back_end/domain/entity/platform.clj
+++ back-end/src/vr_match_back_end/domain/entity/platform.clj
@@ -3,11 +3,21 @@
    [clojure.spec.alpha :as s]
    [clj-time.spec :as t-spec]))
 
+
 (s/def ::id number?)
 (s/def ::name string?)
 (s/def ::url string?)
+(s/def ::platform_user_id string?)
 
 (s/def ::platform
-  (s/keys :req [::id
-                ::name
-                ::url]))
+  (s/keys :req-un [::id
+                   ::name
+                   ::url]
+          :opt-un [::platform_user_id]))
+
+(s/def ::example_platform_user_id ::platform_user_id)
+
+(s/def ::platform-master
+  (s/keys :req-un [::id
+                   ::name
+                   ::example_platform_user_id]))
