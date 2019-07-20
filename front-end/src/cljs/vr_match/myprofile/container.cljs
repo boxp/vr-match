76c68decf916821f8f1973208c411ca44076a25c
boxp <tiyotiyouda@gmail.com>
2019-07-20 15:29:13 +0900

    自分のプロフィール画面のqueryつなぎ込み

--- front-end/src/cljs/vr_match/myprofile/container.cljs
+++ front-end/src/cljs/vr_match/myprofile/container.cljs
@@ -3,23 +3,23 @@
    [reagent.core :as r]
    [re-frame.core :as re-frame]
    [vr-match.events :as events]
+   [vr-match.subs :as subs]
    [vr-match.myprofile.component :as component]
    [vr-match.util :as util]))
+
+(defn- handle-initialize []
+  (re-frame/dispatch [::events/fetch-me {:with-images? true
+                                         :with-platforms? true}]))
 
 (defn- handle-click-edit-my-profile []
   (re-frame/dispatch [::events/push "/mypage"]))
 
-(def myprofile-state (r/atom {:me
-                              {:id 1
-                               :title "サンプル画像"
-                               :userName "一箱"
-                               :introduction "バーチャル清楚系女子高校生Webアプリケーションエンジニアおじさんです。こっそりプログラミングしてます。"
-                               :platForms [{:id 1 :name "VRChat" :link "https://vrchat.net/home/user/usr_3b6403c3-be9f-432c-ab1f-446778946421"} {:id 2 :name "YouTube" :link "https://www.youtube.com/user/BOXPKETARO/about"} {:id 3 :name "VirtualCast" :link ""}]
-                               :image ["https://storage.googleapis.com/boxp-tmp/profile_sample.png"]
-                               :isMatched false}}))
-
 (defn myprofile
   [params]
-  [component/myprofile (merge @myprofile-state {:handleClickEditMyProfile handle-click-edit-my-profile})])
+  (let [me (re-frame/subscribe [::subs/me])]
+    (fn [_]
+      [component/myprofile {:me @me
+                            :handleInitialize handle-initialize
+                            :handleClickEditMyProfile handle-click-edit-my-profile}])))
 
 (util/universal-set-loaded! :myprofile)
