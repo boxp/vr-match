3a4561df64e685e633553ec02f8999c7cdd1e2f4
boxp <tiyotiyouda@gmail.com>
2019-08-10 16:11:15 +0900

    empty画面にHito Hubのシェアボタンを置く

--- front-end/src/cljs/vr_match/approach/components/empty.cljs
+++ front-end/src/cljs/vr_match/approach/components/empty.cljs
@@ -31,6 +31,9 @@
        :color "primary"
        :style {:font-size "10em"}}
      "done"]]
-    [:> js/MaterialUI.Typography {:variant "body1"
-                                  :component "p"}
-     "新たなアバターの登録をお待ちください"]]])
+    [:> js/MaterialUI.Button {:variant "contained"
+                              :color "primary"
+                              :style {:margin-left "auto"
+                                      :margin-right "auto"}
+                              :href "https://twitter.com/intent/tweet?text=Hito%20Hub%E3%81%A7VRChat%E3%81%AE%E3%83%95%E3%83%AC%E3%83%B3%E3%83%89%E3%82%84%E8%87%AA%E5%88%86%E3%81%AE%E3%81%8A%E6%B0%97%E3%81%AB%E5%85%A5%E3%82%8A%E3%81%AE%E3%82%A2%E3%83%90%E3%82%BF%E3%83%BC%E3%82%92%E3%81%95%E3%81%8C%E3%81%97%E3%81%A6%E3%81%BF%E3%82%88%E3%81%86%F0%9F%91%A6%F0%9F%91%A7&url=https://hitohub.boxp.tk&hashtags=hitohub&via=b0xp2"}
+     "Hito Hubを他の人へシェアする"]]])
