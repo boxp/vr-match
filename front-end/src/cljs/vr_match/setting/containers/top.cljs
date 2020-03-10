9289f920fc762b9bf5911a441ba00b7680d79ea0
boxp <tiyotiyouda@gmail.com>
2020-03-11 03:01:13 +0900

    サードパーティ紐づけ画面への遷移を実装

--- front-end/src/cljs/vr_match/setting/containers/top.cljs
+++ front-end/src/cljs/vr_match/setting/containers/top.cljs
@@ -1,10 +1,13 @@
 (ns vr-match.setting.containers.top
   (:require
    [reagent.core :as r]
+   [re-frame.core :as re-frame]
+   [vr-match.events :as events]
    [vr-match.setting.components.top :as components]
    [vr-match.util :as util]))
 
-(defn- handle-click-third-party-setting [])
+(defn- handle-click-third-party-setting []
+  (re-frame/dispatch [::events/push "/setting/third-party-authorization"]))
 
 (defn top
   [_]
