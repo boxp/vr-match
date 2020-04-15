7a1d328364a282fc40ede36f6f1e76e8da5c1d30
boxp <tiyotiyouda@gmail.com>
2020-04-15 23:56:38 +0900

    Revert "fix: SSRサーバーがtwitter-login画面をロードできていない"
    This reverts commit d4e62cde73f7a014279509eba9fcc0e638b01c5b.

--- front-end/src/cljs-server/vr_match/server.cljs
+++ front-end/src/cljs-server/vr_match/server.cljs
@@ -16,7 +16,6 @@
     [vr-match.auth.containers.email-register-complete]
     [vr-match.auth.containers.email-login]
     [vr-match.auth.containers.email-login-complete]
-    [vr-match.auth.containers.twitter-login]
     [vr-match.wizard.container]
     [vr-match.favorite.container]
     [vr-match.matching.container]
